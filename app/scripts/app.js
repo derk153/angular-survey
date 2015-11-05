2e533a5cab153329079484644b963a6b95ac8a1c
alexz <derk153@gmail.com>
2015-11-05 17:57:49 +0200

    add survey

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,16 +11,47 @@
 angular
   .module('appApp', [
     'ngMessages',
-    'ngRoute'
+    'ngRoute',
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
+  .config(function($routeProvider, $stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/main',
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .otherwise({
-        redirectTo: '/'
+      .state('main.create', {
+        url: '/create',
+        templateUrl: 'views/main-create.html'
+      })
+      .state('main.take', {
+        url: '/take',
+        templateUrl: 'views/main-take.html'
+      })
+      .state('main.result', {
+        url: '/result',
+        templateUrl: 'views/main-result.html'
       });
+    $urlRouterProvider.otherwise('/main/create');
+    // $routeProvider
+    //   .when('/', {
+    //     templateUrl: 'views/main.html',
+    //     controller: 'MainCtrl',
+    //     controllerAs: 'main'
+    //   })
+    //   .when('/survey', {
+    //     templateUrl: 'views/survey.html',
+    //     controller: 'SurveyCtrl',
+    //     controllerAs: 'survey'
+    //   })
+    //   .when('/build', {
+    //     templateUrl: 'views/build.html',
+    //     controller: 'BuildCtrl',
+    //     controllerAs: 'build'
+    //   })
+    //   .otherwise({
+    //     redirectTo: '/'
+    //   });
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,66 @@
  * Controller of the appApp
  */
 angular.module('appApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function($scope, $http, surveyService, $state) {
+
+    $scope.types = [{
+      name: 'Radio buttons',
+      value: 'radio'
+    }, {
+      name: 'Checkbxes',
+      value: 'checkbox'
+    }, {
+      name: 'Drop-down',
+      value: 'dropdown'
+    }, {
+      name: 'Input',
+      value: 'input'
+    }];
+
+    $scope.formData = {name: ''};
+    $scope.result = {};
+    $scope.questions = [];
+
+    var defaultQuestions = {
+      type: $scope.types[0].value,
+      options: []
+    };
+
+    $scope.question = defaultQuestions;
+
+    $scope.addQuestion = function() {
+      var question = $scope.question;
+
+      $scope.questions.push(question);
+      $scope.question = {
+        type: $scope.types[0].value,
+        options: []
+      };
+    };
+
+    $scope.removeTodo = function(index) {
+      $scope.questions.splice(index, 1);
+    };
+
+    $scope.addField = function(item) {
+      item.push('');
+    };
+    $scope.removeField = function(item, index) {
+      item.splice(index, 1);
+    };
+
+    $scope.submitSurvey = function(data){
+      // surveyService.addSurvey(data);
+      $scope.formData.questions = data;
+      // $http.get('data/questions.json').success(function(data) {
+      //   $scope.formData.questions = data;
+      // });
+    };
+
+    $scope.addSurvey = function () {
+      // var result = JSON.stringify($scope.formData);
+      $scope.result = $scope.formData;
+      $state.go('main.result');
+    };
+
   });
